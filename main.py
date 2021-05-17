aec5e5afd57395a0e94df4a47a7bc3f55e7927a3
Angela-Sanchez-Guitart <ansanguit@gmail.com>
2021-05-17 22:50:46 +0100

    Changed data to json and added search button

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -47,21 +48,62 @@ def generate_password():
 
 
 def add_password():
+
     # how to create pop ups. Standard dialogs - message boxes
-    if len(website_entry.get()) == 0 or len(password_entry.get()) == 0:
+    website = website_entry.get()
+    email = user_entry.get()
+    password = password_entry.get()
+    new_data={
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
+    if len(website) == 0 or len(password) == 0:
         messagebox.showerror(title= "Oops", message="Please make sure you have entered a password and website")
     else:
-       is_ok =messagebox.askokcancel(title=website_entry.get(), message= f"These are the details entered  {user_entry.get()} | {password_entry.get()} is this ok to save?")
+       is_ok =messagebox.askokcancel(title=website, message= f"These are the details entered  {email} | {password} is this ok to save?")
+
        if is_ok:
-          f = open("password_db.txt", "a")
-          f.write(f"{website_entry.get()} | {user_entry.get()} | {password_entry.get()}\n")
-          f.close()
-          messagebox.showinfo(title="Hurray", message=f"Password for {website_entry.get()} successfully saved")
-          print(f"Password for {website_entry.get()} successfully saved")
-          website_entry.delete(4, END)
-          password_entry.delete(0, END)
+           try:
+               with open("data.json", "r") as data_file:
+                   data = json.load(data_file)
+           except FileNotFoundError:
+               with open("data.json", "w") as data_file:
+                   json.dump(new_data, data_file, indent =4)
+           else:
+               #updating with the new data
+               data.update(new_data)
+               with open("data.json", "w") as data_file:
+                   # saving the updated data
+                   json.dump(data, data_file, indent=4)
+
+          # f = open("password_db.txt", "a")
+          # f.write(f"{website_entry.get()} | {user_entry.get()} | {password_entry.get()}\n")
+          # f.close()
+           finally:
+               messagebox.showinfo(title="Hurray", message=f"Password for {website} successfully saved")
+               print(f"Password for {website} successfully saved")
+               website_entry.delete(4, END)
+               password_entry.delete(0, END)
 
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+def find_password():
+    website_search = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data =json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo("Error", message="No data file found.")
+    else:
+        if website_search in data:
+            email = data[website_search]["email"]
+            password = data[website_search]["password"]
+            messagebox.showinfo(title=website_search, message=f"Email={email} \n Password: {password}")
+        else:
+            messagebox.showinfo(title= "Error", message=f"No password saved for {website_search}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -90,20 +132,23 @@ password.grid(column=0, row=3)
 web = StringVar()
 website_entry = Entry(width = 35, textvariable = web)
 website_entry.focus() # this gets the cursor on this entry when we open
-website_entry.grid(column=1, row= 1, columnspan =2, sticky="w")
+website_entry.grid(column=1, row= 1, columnspan =1, sticky="w")
 
 user_entry= Entry(width= 45, font= FONT)
 user_entry.insert(0, "ansanguit@gmail.com") #this is to prepopulate the user
 user_entry.grid(column=1, row= 2, columnspan =2, sticky= "w") # columnspan to take more than one column
 
-password_entry = Entry(width=28, font= FONT)
+password_entry = Entry(width=45, font= FONT)
 password_entry.grid(column=1, row= 3, sticky="w")
 
-generate = Button(text="Generate Password", font= FONT, command= generate_password, fg= "pink", activebackground="black")
-generate.grid(column=2, row = 3, sticky="w")
+generate = Button(text="Generate Password", font= FONT, command= generate_password, fg= "green", activebackground="black")
+generate.grid(column=2, row = 3)
 
-add= Button(width = 36, text="Add", font=FONT, fg= "pink", activebackground="black", command= add_password)
+add= Button(width = 30, text="Add", font=FONT, fg= "pink", activebackground="black", command= add_password)
 add.grid(column= 1, row= 4, columnspan=2)  # sticky to align the label, entry or button
+
+search = Button(width = 17, text="Search", font=FONT, fg= "green", activebackground="black", command= find_password)
+search.grid(column= 2, row= 1)  # sticky to align the label, entry or button
 
 
 
